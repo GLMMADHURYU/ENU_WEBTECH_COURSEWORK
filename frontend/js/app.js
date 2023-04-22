db0d745a12d5ffab6c3e7be98f585c614376628c
GLMMADHURYU <130765080+GLMMADHURYU@users.noreply.github.com>
2023-04-22 08:26:14 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,17 @@
-
+function validateForm() {
+  var name = document.forms["myForm"]["name"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var message = document.forms["myForm"]["message"].value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (message == "") {
+    alert("Message must be filled out");
+    return false;
+  }
+}
